fe3d5b969b7e273688a7ef29f269a17b211fca9d
Sunjay Varma <varma.sunjay@gmail.com>
2015-09-22 22:44:47 -0400

    Added some more functions to wheel

--- wheel.hs
+++ wheel.hs
@@ -24,11 +24,16 @@ take' n (x:xs) = x : take' (n-1) xs
 
 -- reverse
 reverse' :: [a] -> a
+reverse' [] = []
+reverse' (x:xs) = reverse' xs ++ [x]
 
 -- repeat
 repeat' :: a -> [a]
+repeat' x = x:repeat' x
 
 -- zip
 zip' :: [a] -> [b] -> [(a, b)]
-
+zip' [] _ = []
+zip' _ [] = []
+zip' (x:xs) (y:ys) = (x, y):zip' xs ys
 
