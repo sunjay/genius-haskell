8c4f3e46af5c0bd6b6acea346eb33177d29891dc
Sunjay Varma <varma.sunjay@gmail.com>
2015-09-29 14:24:15 -0400

    Added a basic winner function

--- genius.hs
+++ genius.hs
@@ -1,3 +1,8 @@
 -- A Genius Tic-Tac-Toe implementation in Haskell --
 -- Author: Sunjay Varma --
 
+-- gets the winner in this row or 0 if there is no winner
+-- using 1 as x and -1 as o and 0 as empty tile
+winner :: (Eq a, Num a) => [a] -> a
+winner row = foldl1 (\acc x -> if acc == x then x else 0) row
+
