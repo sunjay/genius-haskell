02529f4732d15da63943ed707ea3259acd290d79
Sunjay Varma <varma.sunjay@gmail.com>
2015-09-30 23:58:37 -0400

    More work on making this work

--- genius.hs
+++ genius.hs
@@ -27,13 +27,30 @@ data GeniusTicTacToe = GeniusTicTacToe {
 
 -- Create a new game
 new :: GeniusTicTacToe
-new = GeniusTicTacToe {gameBoards=replicate size $ replicate size boardNew, gameWinner=Nothing}
+new = GeniusTicTacToe {gameBoards=replicate size $ replicate size newBoard, gameWinner=Nothing}
     where size = 3
 
 -- Creates a new board
-boardNew :: TicTacToe
-boardNew = TicTacToe {boardTiles=replicate size $ replicate size Nothing, boardWinner=Nothing}
+newBoard :: TicTacToe
+newBoard = TicTacToe {boardTiles=replicate size $ replicate size Nothing, boardWinner=Nothing}
     where size = 3
+
+boardRow :: TicTacToe -> Int -> [Maybe Piece]
+boardRow board row = boardTiles board !! row
+
+-- Gets a single tile value from a board
+boardTile :: TicTacToe -> Int -> Int -> Maybe Piece
+boardTile board row col = boardRow board row !! col
+
+-- Makes a move on a board and then returns a new board
+boardMove :: TicTacToe -> Int -> Int -> Piece -> TicTacToe
+boardMove board row col piece
+    | boardTile board row col == Nothing = replaceNth row (replaceNth col piece (boardRow board row)) (boardTiles board)
+
+replaceNth :: Int -> a -> [a] -> [a]
+replaceNth n newValue (x:xs)
+    | n == 0 = newValue:xs
+    | otherwise = x:replaceNth (n-1) newValue xs
 
 -- gets the winner in this row or 0 if there is no winner
 -- using 1 as x and -1 as o and 0 as empty tile
