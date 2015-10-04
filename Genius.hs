a4cfbf653be83ee62a8b68969845923a263c10e0
Sunjay Varma <varma.sunjay@gmail.com>
2015-10-03 22:57:44 -0400

    Renamed files to match haskell naming conventions for imports

---
+++ Genius.hs
@@ -0,0 +1,32 @@
+-- A Genius Tic-Tac-Toe implementation in Haskell --
+-- Author: Sunjay Varma --
+
+module Genius (
+    TicTacToe,
+    new,
+    boards,
+    winner,
+    board,
+--    move,
+    board_size
+) where
+
+import qualified TicTacToe as T
+import TicTacToe (TicTacToe, Piece (PieceX, PieceO), board_size)
+
+data GeniusTicTacToe = GeniusTicTacToe {
+    boards :: [TicTacToe],
+    winner :: Maybe Piece
+} deriving (Show)
+
+-- Create a new game
+new :: GeniusTicTacToe
+new = GeniusTicTacToe {boards=replicate (board_size*board_size) T.new, winner=Nothing}
+
+board :: Int -> Int -> GeniusTicTacToe -> TicTacToe
+board rowIndex colIndex game
+    | rowIndex < board_size && colIndex < board_size = boards game !! (rowIndex * board_size + colIndex)
+
+--move :: Int -> Int -> GeniusTicTacToe -> GeniusTicTacToe
+
+
