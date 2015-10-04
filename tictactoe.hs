ba9fe887f96ae04f1ce4871e2b224b8de666e8c2
Sunjay Varma <varma.sunjay@gmail.com>
2015-10-03 20:40:53 -0400

    Implemented move function

--- tictactoe.hs
+++ tictactoe.hs
@@ -15,6 +15,8 @@ module TicTacToe (
     --diagonalTRBL
 ) where
 
+import Data.Maybe (isNothing)
+
 board_size = 3
 
 data Piece = PieceX | PieceO deriving (Show, Eq)
@@ -48,10 +50,22 @@ diagonalTRBL board = map (\n -> tile n (board_size-n) board) [0..board_size-1]
 
 -- Extracts a single tile from the board
 tile :: Int -> Int -> TicTacToe -> Maybe Piece
-tile rowIndex colIndex board = tiles board !! (rowIndex * board_size + colIndex)
+tile rowIndex colIndex board
+    | rowIndex < board_size && colIndex < board_size = tiles board !! (rowIndex * board_size + colIndex)
 
 -- Makes a move on the board
---move :: Int -> Int -> Piece -> TicTacToe -> TicTacToe
+move :: Int -> Int -> Maybe Piece -> TicTacToe -> TicTacToe
+move rowIndex colIndex piece board
+    | rowIndex < board_size && colIndex < board_size && not (isNothing piece) && isNothing (winner board)
+        = TicTacToe {
+            tiles=map filterTiles $ zip [0..] $ tiles board,
+            winner=winner board
+        }
+        where filterTiles = (\(index, value) ->
+                if (rowIndex * board_size + colIndex) == index then 
+                    piece
+                else
+                    value)
 
 -- Takes each nth element from a list
 each n = map head . takeWhile (not . null) . iterate (drop n)
