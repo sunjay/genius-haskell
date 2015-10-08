2cd908d3f1132de91f638485e461cb484d911d28
Sunjay Varma <varma.sunjay@gmail.com>
2015-10-08 01:13:29 -0400

    Started writing down some ideas for tests

---
+++ TicTacToeSpec.hs
@@ -0,0 +1,4 @@
+-- Test that moving on a board that has already been won does not change the winner
+
+-- Test that moving on a tile actually results in the tile function returning the correct value
+-- Make sure this isn't prone to any errors (so always start with an empty board before placing each tile)
